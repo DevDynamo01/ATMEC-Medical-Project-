2bf848ab2942dfbd9e19e24eb8b08ca7cfcea7ab
Sagar1917118 <112215153@cse.iiitp.ac.in>
2024-10-12 18:27:34 +0530

    chat section first commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import { ChatContextProvider } from './context/chatContext';
+import SideBar from './components/SideBar';
+import ChatView from './components/ChatView';
+import { useEffect, useState } from 'react';
+import Modal from './components/Modal';
+import Setting from './components/Setting';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ChatContextProvider>
+      <div className="flex transition duration-500 ease-in-out">
+        <SideBar />
+        <ChatView />
+      </div>
+    </ChatContextProvider>
     </div>
   );
 }
