5d0b43c03fcdcd3a025943594bca2884ba5a5e1f
Sagar1917118 <112215153@cse.iiitp.ac.in>
2024-10-17 01:21:50 +0530

    final commit updating logos

--- my-app/src/components/ResponsiveAppBar.jsx
+++ my-app/src/components/ResponsiveAppBar.jsx
@@ -12,7 +12,7 @@ import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
-
+import logo from "../assets/logo2.png";
 const pages = [];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
@@ -39,25 +39,7 @@ function ResponsiveAppBar() {
     <AppBar position="static">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="/"
-            sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            MED-GEN AI
-          </Typography>
-
+         <img src={logo} className='h-[50px]'></img>
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
             <IconButton
               size="large"
