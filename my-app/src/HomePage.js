b6e88379c420a157f0343a69c46281d81eb81271
sahilsingh84 <singhsahil840060@gmail.com>
2024-10-24 22:50:09 +0530

    updating-home-page-and-add-features-button

--- my-app/src/HomePage.js
+++ my-app/src/HomePage.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { useRef, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import HomeButton from './components/HomeButtons';
+import { TypeAnimation } from 'react-type-animation';
+import FeatureButtonJSON from './FeatureButtonData/FeatureButtonJSON.json';
+import FeaturesButtonCard from './FeaturesButtonCard';
 const HomePage = () => {
   const videoRef = useRef(null);
   const navigate = useNavigate();
@@ -18,7 +20,50 @@ const HomePage = () => {
 
   return (
     <div className="homePage">
-      <div className="imageComponent">
+      <div
+        style={{
+          position: 'relative',
+          width: '100%',
+          minHeight: '700px',
+        }}
+      >
+        <div className="homepage-background-image">
+          <div className="description">
+            <h3>
+              <span className="Homepageheading">MEDGEN AI: </span>
+              <TypeAnimation
+                sequence={['Chat Your Cure', 3000, '']}
+                repeat={Infinity}
+                // omitDeletionAnimation={true}
+              ></TypeAnimation>
+            </h3>
+            <p>
+              MedGen AI uses cutting-edge AI to provide personalized medical insights from genetic
+              data, helping patients and doctors make smarter, data-driven decisions for better
+              health outcomes.
+            </p>
+          </div>
+          <div className="videoFile">
+            <video ref={videoRef} src="medical.mp4" muted loop />
+          </div>
+        </div>
+      </div>
+      <div className="cardCompnent">
+        {FeatureButtonJSON.map((feature, index) => {
+          return <FeaturesButtonCard key={index} feature={feature} />;
+        })}
+      </div>
+
+      <div className="homepageChatBot" onClick={() => navigate('/chatbot')}>
+        <img src="chatbot.png" />
+      </div>
+      {/* {FeatureButtonJSON.map((data, key) => {});
+      <div className="cardCompnent">
+        {data.imageURL}
+        <FeaturesButtonCard />
+      </div>
+     } */}
+      {/* <div className="imageComponent">
         <div className="heading text-black">Chat Your Cure</div>
         <p className="homePagePara text-black">
           MedGen AI uses cutting-edge AI to provide personalized medical insights from genetic data,
@@ -26,8 +71,6 @@ const HomePage = () => {
           outcomes.
         </p>
         <div className="chatButton flex flex-col gap-10">
-          {/* <button className="navigateButtons" onClick={()=>{navigate("/chatbot")}}>Start Chat</button>
-        <button className="navigateButtons" onClick={()=>{navigate("/diagonsis")}}>Disease Diagonsis</button> */}
           <HomeButton text={'Start Chat'} path={'/chatbot'}></HomeButton>
           <HomeButton text={'Disease Diagonsis'} path={'/diagonsis'}></HomeButton>
           <HomeButton text={'Generate DataSet By Description'} path={'/generateData'}></HomeButton>
@@ -39,7 +82,7 @@ const HomePage = () => {
           <HomeButton text={'Analyze Medical Documents'} path={'/handle-uploader'}></HomeButton>
         </div>
         <video ref={videoRef} src="medical.mp4" muted loop />
-      </div>
+      </div> */}
     </div>
   );
 };
